83614831bad3237f7d0867f896f12f75e070770f
Marco Gnecchi <marco.gnecchi@icloud.com>
2018-05-31 17:51:45 +0200

    allineamento

--- src/rivalutazioni2Excel.php
+++ src/rivalutazioni2Excel.php
@@ -69,35 +69,36 @@
 
         // riquadro di testata
         // --------------------------------------------------------------------------------
-        $sheet->setCellValue('A1', strtoupper('fornitore'));
-        $sheet->setCellValue('B1', $rivalutazione['fornitore']);
-        $sheet->mergeCells('B1:G1');
-        $sheet->setCellValue('A2', strtoupper('descrizione'));
-        $sheet->setCellValue('B2', $rivalutazione['descrizione']);
-        $sheet->mergeCells('B2:G2');
-        $sheet->setCellValue('A3',strtoupper('data'));
-        $sheet->setCellValue('B3', Date::PHPToExcel($data->setTimezone($timeZone)->format('Y-m-d')));
-		$sheet->getStyle('B3')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_DDMMYYYY);
-		$sheet->mergeCells('B3:G3');
+        $sheet->setCellValue('A1', strtoupper('numero'));
+        $sheet->setCellValue('B1', $rivalutazione['numero']);
+        $sheet->mergeCells('B1:H1');
+    	$sheet->setCellValue('A2',strtoupper('data'));
+        $sheet->setCellValue('B2', Date::PHPToExcel($data->setTimezone($timeZone)->format('Y-m-d')));
+		$sheet->getStyle('B2')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_DDMMYYYY);
+		$sheet->mergeCells('B2:H2');
+        $sheet->setCellValue('A3', strtoupper('descrizione'));
+        $sheet->setCellValue('B3', $rivalutazione['descrizione']);
+        $sheet->mergeCells('B3:H3');
         $sheet->setCellValue('A4', strtoupper('fornitore'));
 		$sheet->setCellValue('B4', $rivalutazione['fornitore']);
-        $sheet->mergeCells('B4:G4');
+        $sheet->mergeCells('B4:H4');
         $sheet->setCellValue('A5', strtoupper('linea'));
 		$sheet->setCellValue('B5', $rivalutazione['linea']);
-		$sheet->mergeCells('B5:G5');
+		$sheet->mergeCells('B5:H5');
         $sheet->setCellValue('A6', strtoupper('valore'));
-        $sheet->mergeCells('B6:G6');
-        $sheet->mergeCells('A7:G7');
+        $sheet->mergeCells('B6:H6');
+        $sheet->mergeCells('A7:H7');
 
         // testata colonne
         // --------------------------------------------------------------------------------
         $sheet->setCellValue('A8', strtoupper('cod. art.'));
         $sheet->setCellValue('B8', strtoupper('cod. art. forn.'));
-        $sheet->setCellValue('C8', strtoupper('descrizione'));
-        $sheet->setCellValue('D8', strtoupper('modello'));
-        $sheet->setCellValue('E8', strtoupper('giacenza'));
-		$sheet->setCellValue('F8', strtoupper('val. un.'));
-        $sheet->setCellValue('G8', strtoupper('valore'));
+        $sheet->setCellValue('C8', strtoupper('barcode'));
+        $sheet->setCellValue('D8', strtoupper('descrizione'));
+        $sheet->setCellValue('E8', strtoupper('modello'));
+        $sheet->setCellValue('F8', strtoupper('giacenza'));
+		$sheet->setCellValue('G8', strtoupper('val. un.'));
+        $sheet->setCellValue('H8', strtoupper('valore'));
 
         // scrittura righe
         // --------------------------------------------------------------------------------
@@ -114,16 +115,17 @@
             $R = ($i+$primaRigaDati);
             
             // formule
-            $valore = "=ROUND(E$R*F$R,2)";
+            $valore = "=ROUND(F$R*G$R,2)";
             
             // righe
 			$sheet->getCell('A'.$R)->setValueExplicit($righe[$i]['codiceArticolo'],DataType::TYPE_STRING);
             $sheet->getCell('B'.$R)->setValueExplicit($righe[$i]['codiceArticoloFornitore'],DataType::TYPE_STRING);
-            $sheet->getCell('C'.$R)->setValueExplicit($righe[$i]['descrizioneArticolo'],DataType::TYPE_STRING);
-            $sheet->getCell('D'.$R)->setValueExplicit($righe[$i]['modelloArticolo'],DataType::TYPE_STRING);
-            $sheet->getCell('E'.$R)->setValueExplicit($righe[$i]['giacenza'],DataType::TYPE_NUMERIC);
-            $sheet->getCell('F'.$R)->setValueExplicit($righe[$i]['valoreUnitario'],DataType::TYPE_NUMERIC);
-            $sheet->getCell('G'.$R)->setValueExplicit($valore,DataType::TYPE_FORMULA);
+            $sheet->getCell('C'.$R)->setValueExplicit($righe[$i]['barcode'],DataType::TYPE_STRING);
+            $sheet->getCell('D'.$R)->setValueExplicit($righe[$i]['descrizioneArticolo'],DataType::TYPE_STRING);
+            $sheet->getCell('E'.$R)->setValueExplicit($righe[$i]['modelloArticolo'],DataType::TYPE_STRING);
+            $sheet->getCell('F'.$R)->setValueExplicit($righe[$i]['giacenza'],DataType::TYPE_NUMERIC);
+            $sheet->getCell('G'.$R)->setValueExplicit($righe[$i]['valoreUnitario'],DataType::TYPE_NUMERIC);
+            $sheet->getCell('H'.$R)->setValueExplicit($valore,DataType::TYPE_FORMULA);
         }
 
         // riquadro di testata (formule)
@@ -131,7 +133,7 @@
         $highestRow = $sheet->getHighestRow();
 		$highestColumn = $sheet->getHighestColumn();
 		
-        $totale = "=SUM(G$primaRigaDati:G$highestRow)";
+        $totale = "=SUM(H$primaRigaDati:H$highestRow)";
         $sheet->getCell('B6')->setValueExplicit($totale,DataType::TYPE_FORMULA);
         $sheet->getStyle("B6")->getNumberFormat()->setFormatCode('###,###,##0.00;[Red][<0]-###,###,##0.00;');
     
@@ -144,31 +146,33 @@
         $sheet->setShowGridlines(true);
 
         // riquadro di testata
-        $sheet->getStyle('A1:G7')->getAlignment()->setHorizontal('left');
+        $sheet->getStyle('A1:H7')->getAlignment()->setHorizontal('left');
 		$sheet->getStyle('A1:A7')->getFont()->setBold(true);
         
 		// testata colonne
-		$sheet->getStyle('A8:G8')->getAlignment()->setHorizontal('center');
-		$sheet->getStyle('A8:G8')->getFont()->setBold(true);
+		$sheet->getStyle('A8:H8')->getAlignment()->setHorizontal('center');
+		$sheet->getStyle('A8:H8')->getFont()->setBold(true);
 
-        $sheet->getStyle("E$primaRigaDati:E$highestRow")->getNumberFormat()->setFormatCode('###,###,##0;[Red][<0]-###,###,##0;');
-		$sheet->getStyle("F$primaRigaDati:F$highestRow")->getNumberFormat()->setFormatCode('###,###,##0.00;[Red][<0]-###,###,##0.00;');
+        $sheet->getStyle("F$primaRigaDati:F$highestRow")->getNumberFormat()->setFormatCode('###,###,##0;[Red][<0]-###,###,##0;');
 		$sheet->getStyle("G$primaRigaDati:G$highestRow")->getNumberFormat()->setFormatCode('###,###,##0.00;[Red][<0]-###,###,##0.00;');
+		$sheet->getStyle("H$primaRigaDati:H$highestRow")->getNumberFormat()->setFormatCode('###,###,##0.00;[Red][<0]-###,###,##0.00;');
 		
 		$sheet->getStyle("A$primaRigaDati:A$highestRow")->getAlignment()->setHorizontal('center');
 		$sheet->getStyle("B$primaRigaDati:B$highestRow")->getAlignment()->setHorizontal('center');
-		$sheet->getStyle("E$primaRigaDati:E$highestRow")->getAlignment()->setHorizontal('center');
-		$sheet->getStyle("F$primaRigaDati:F$highestRow")->getAlignment()->setHorizontal('right');
+		$sheet->getStyle("C$primaRigaDati:C$highestRow")->getAlignment()->setHorizontal('center');
+		$sheet->getStyle("F$primaRigaDati:F$highestRow")->getAlignment()->setHorizontal('center');
 		$sheet->getStyle("G$primaRigaDati:G$highestRow")->getAlignment()->setHorizontal('right');
+		$sheet->getStyle("H$primaRigaDati:H$highestRow")->getAlignment()->setHorizontal('right');
 
         // larghezza colonne (non uso volutamente autowidth)
         $sheet->getColumnDimension('A')->setWidth(15);
         $sheet->getColumnDimension('B')->setWidth(20);
-        $sheet->getColumnDimension('C')->setWidth(60);
-        $sheet->getColumnDimension('D')->setWidth(20);
-        $sheet->getColumnDimension('E')->setWidth(12);
+        $sheet->getColumnDimension('C')->setWidth(25);
+        $sheet->getColumnDimension('D')->setWidth(60);
+        $sheet->getColumnDimension('E')->setWidth(20);
         $sheet->getColumnDimension('F')->setWidth(12);
         $sheet->getColumnDimension('G')->setWidth(12);
+        $sheet->getColumnDimension('H')->setWidth(12);
         
         $rigaTitoli = $primaRigaDati - 1;
         $styleArray = array(
@@ -179,7 +183,7 @@
             	),
         	),
     	);
-    	$sheet ->getStyle("A$rigaTitoli:G$rigaTitoli")->applyFromArray($styleArray);
+    	$sheet ->getStyle("A$rigaTitoli:H$rigaTitoli")->applyFromArray($styleArray);
         $sheet ->getStyle("A$rigaTitoli:A$highestRow")->applyFromArray($styleArray);
         $sheet ->getStyle("B$rigaTitoli:B$highestRow")->applyFromArray($styleArray);
         $sheet ->getStyle("C$rigaTitoli:C$highestRow")->applyFromArray($styleArray);
@@ -187,6 +191,7 @@
         $sheet ->getStyle("E$rigaTitoli:E$highestRow")->applyFromArray($styleArray);
         $sheet ->getStyle("F$rigaTitoli:F$highestRow")->applyFromArray($styleArray);
         $sheet ->getStyle("G$rigaTitoli:G$highestRow")->applyFromArray($styleArray);
+        $sheet ->getStyle("H$rigaTitoli:H$highestRow")->applyFromArray($styleArray);
 		
 		$sheet->getPageSetup()->setOrientation(PageSetup::ORIENTATION_LANDSCAPE);
 		$sheet->getPageSetup()->setPaperSize(PageSetup::PAPERSIZE_A4);
